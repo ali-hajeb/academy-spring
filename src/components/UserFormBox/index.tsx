5c3a897a822edbf19e11a2b38620b257ffd70b13
Ali Hajeb <ali.hajeb@hotmail.com>
2022-11-22 21:13:41 +0330

    Added class attribute

--- src/components/UserFormBox/index.tsx
+++ src/components/UserFormBox/index.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 
 export interface UserFormBoxProps extends React.PropsWithChildren {
   formSubmitHandler: (event: React.FormEvent<HTMLFormElement>) => void;
+  formClassName?: string;
 }
 
 const UserFormBox: React.FunctionComponent<UserFormBoxProps> = ({
   children,
+  formClassName,
   formSubmitHandler,
 }) => {
   return (
     <section className='flex justify-center items-center h-full w-full'>
-      <div className='py-2 px-2 w-full sm:w-[300px]'>
-        <form onSubmit={formSubmitHandler}>{children}</form>
+      <div className='py-2 px-2 w-full sm:w-[400px]'>
+        <form className={formClassName} onSubmit={formSubmitHandler}>{children}</form>
       </div>
     </section>
   );
