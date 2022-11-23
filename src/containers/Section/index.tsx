453f4390ebe19ae3a2c86fc1fef21e2a6a19efe2
Ali Hajeb <ali.hajeb@hotmail.com>
2022-11-23 22:56:08 +0330

    Added section container

---
+++ src/containers/Section/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export interface SectionProps extends React.PropsWithChildren {
+  title: string
+  className?: string
+}
+
+const Section: React.FunctionComponent<SectionProps> = ({ title, className, children }) => {
+  return ( <section className={className}>
+    <h1 className='text-center font-bold text-3xl'>
+      {title}
+    </h1>
+    {children}
+  </section> );
+}
+
+export default Section;
