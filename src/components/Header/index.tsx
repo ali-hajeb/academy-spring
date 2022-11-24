95a9206650abcf67156179278ded0f3f9d536668
Ali Hajeb <ali.hajeb@hotmail.com>
2022-11-24 17:04:35 +0330

    Updated

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,9 @@
-import React, { useState } from 'react';
-import Navbar from '../../components/Navbar/Navbar';
-import { INavItem } from '../../components/Navbar/types';
-import { useAppSelector } from '../../store';
+import React from 'react';
+export interface HeaderProps extends React.PropsWithChildren {
+  navbar?: React.ReactNode;
+}
 
-const navbarItems: INavItem[] = [
-  { id: '1', title: 'خانه', to: '/' },
-  { id: '2', title: 'تماس با ما', to: '/contact-us' },
-];
-
-export interface HeaderProps {}
-
-const Header: React.FunctionComponent<HeaderProps> = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
-  const isUserLoggedIn = useAppSelector((state) => state.user.isLoggedIn);
-
+const Header: React.FunctionComponent<HeaderProps> = ({ children, navbar }) => {
   return (
     <div className="isolate bg-white">
       <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
@@ -44,57 +33,19 @@ const Header: React.FunctionComponent<HeaderProps> = () => {
           </defs>
         </svg>
       </div>
-      <div className="px-6 pt-6 lg:px-8">
-        <div>
-          <Navbar
-            items={navbarItems}
-            isUserLoggedIn={isUserLoggedIn}
-            mobileMenuOpen={mobileMenuOpen}
-            setMobileMenuOpen={setMobileMenuOpen}
-          />
+      {navbar && (
+        <div className="px-6 pt-6 lg:px-8">
+          <div>
+            {navbar}
+          </div>
         </div>
-      </div>
+      )}
       <div>
         <div className="relative px-6 lg:px-8">
           <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-32 sm:pb-40">
             <div>
               <div>
-                <div className="academy-logo flex items-center justify-center mb-4">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="rgb(79, 70, 229)"
-                    className="w-[128px] h-[128px]"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z"
-                    />
-                  </svg>
-                </div>
-                <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
-                  آکادمی بهار
-                </h1>
-                <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
-                  فضایی برای یادگیری، رشد حرفه‌ای و همگرایی توسعه‌دهندگان
-                </p>
-                <div className="mt-8 flex gap-x-4 sm:justify-center">
-                  <a
-                    href="#"
-                    className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
-                  >
-                    دوره‌ها
-                  </a>
-                  <a
-                    href="#"
-                    className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-                  >
-                    درباره‌ی ما
-                  </a>
-                </div>
+                {children}
               </div>
               <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
                 <svg
