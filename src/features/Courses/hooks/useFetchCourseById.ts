57f35952dc95b062d2e37c8f7c8eb3bc8e98d50f
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-11-25 19:10:04 +0330

    modification: pagination and course action integratin with modern ui

--- src/features/Courses/hooks/useFetchCourseById.ts
+++ src/features/Courses/hooks/useFetchCourseById.ts
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
+import {  useEffect, useState } from 'react';
 import { ICourse } from '../types/courses';
 import courseRequest from '../services/courseService';
-import { useAppSelector } from '../../../store';
+import IUser from '../../UserAuthentication/types/user';
 
 
 interface IInitialState{
   status: 'idle' | 'loading' | 'succeeded' | 'failed';
-  error?: { message:string} | undefined;
-  payload?:ICourse |undefined;
+  error: { message:string} | undefined;
+  payload:ICourse;
   isLiked: boolean,
-  count: {like:number, disLike:number},
+  count: number,
   isStudentEnrolled: boolean
 }
 
@@ -20,81 +20,119 @@ export const useFetchCourseById = (id:string)=>{
   const initialState:IInitialState = {
     status: 'idle',
     error:undefined,
-    payload:undefined,
+    payload:{} as ICourse,
     isLiked:false,
-    count:{like:0, disLike:0},
+    count:0,
     isStudentEnrolled: false
   };
  const [course, setCourse] = useState<IInitialState>({...initialState});
- const user = useAppSelector(state=>state.user);
-  useEffect(()=>{ 
-    setCourse({...course, status: 'loading'});
-    courseRequest.getCoursesById(id).then((res)=>{
-      const data = JSON.parse(res.data);
-      const isStudentEnrolled =  data.result.students.find((student:any)=>{
-        return student.id === user._id;
-      });
-      console.log(isStudentEnrolled);
-      
-      setCourse({ ...course, status:'succeeded', payload:data.result, isStudentEnrolled })
-      
-    }).catch(error=>{
-      setCourse({...course,status:'failed', error:{message:error.message}})
-    })
-  },[]);
+//  const user = useAppSelector(state=>state.user);
+ useEffect(()=>{ 
+  const getCourseData = async()=>{
+
+
+try {
+const res = await courseRequest.getCoursesById(id);
+const data = JSON.parse(res.data);
+const isStudentEnrolled =  data.result.students.find((student:any)=>{
+  const user = JSON.parse(localStorage.getItem('user_data') ?? '{}') as IUser;
+  return student._id === user?._id;
+});
+
+try {
+const res = await courseRequest.likeCourse(id);
+const countRes =await courseRequest.countLike(id);
+const {like, dislike} = countRes.data.result
+if (res.data.message[0].message === "شما قبلا این دوره را لایک کردید"){
+  try {
+    
+    if (countRes.data.success) {
+     
+     setCourse({...course, count: +like - +dislike, status:'succeeded', payload:data.result, isStudentEnrolled:!!isStudentEnrolled, isLiked:true})
+   }
+   } catch (error) {
+     console.log(error);
+     
+   }
+} else{
+  try {
+    const res= await courseRequest.disLikeCourse(id);
+    
+  if (res.data.message[0].message !== "شما قبلا این دوره را دیسلایک کردید") {
+  try {
+  
+   if (countRes.data.success) {
+    
+    setCourse({...course, count: +like - +dislike, status:'succeeded', payload:data.result, isStudentEnrolled:!!isStudentEnrolled, isLiked:false})
+  }
+  } catch (error) {
+    console.log(error);
+    
+  }
+  }
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+} catch (error) {
+console.log(error);
+
+}
+} catch (error:any) {
+
+setCourse({...course,status:'failed', error:{message:error.message}});
+
+} 
+  }
+  getCourseData();
+// eslint-disable-next-line react-hooks/exhaustive-deps
+},[]);
+
+
+
+
 
   const likeCourse = async()=>{
+    setCourse({...course, isLiked: true, count: course.count + 1})
     try {
-      setCourse({...course, isLiked:true})
       const res = await courseRequest.likeCourse(id);
-      console.log('try', res);
+     if (!res.data.success) {
+      setCourse({...course, isLiked: false});
       
-     if (!res.data.success && res.data.message[0].message !== "شما قبلا این دوره را لایک کردید") {
-      setCourse({...course, isLiked: false})
     }
     } catch (error) {
       console.log(error);
-      
+      setCourse({...course, isLiked: false , count: course.count - 1});
     }
-    try {
-      const countRes =await courseRequest.countLike(id);
-  
-  if (countRes.data.success) {
-    setCourse({...course, count: countRes.data.result})
-  }
-} catch (error) {
-  console.log(error);
-}
     
   }
   const disLikeCourse = async()=>{
-    try{
-      console.log(id, user._id);
-      setCourse({...course, isLiked:false})
+
+   setCourse({...course, isLiked:false, count: course.count - 1})
+    try{      
     const res = await courseRequest.disLikeCourse(id);
 
-    if (!res.data.success && res.data.message[0].message !== "شما قبلا این دوره را دیسلایک کردید") {
-      setCourse({...course, isLiked: true})
+
+    if (!res.data.success) {
+      setCourse({...course, isLiked: true});
+   
     }
-    console.log(res,'try');
-    
+
   } catch (error) {
     console.log(error);
-    
+    setCourse({...course, isLiked: true, count: course.count + 1});
   }
-  try {
-        const countRes =await courseRequest.countLike(id);
-    
-    if (countRes.data.success) {
-      setCourse({...course, count: countRes.data.result})
-    }
-  } catch (error) {
-    console.log(error);
-  }
-  }
-  const addStudentToCourse = async()=>{
+  
+  };
+
+const toggleLike = async()=>{    
+ course.isLiked ? await disLikeCourse() : await likeCourse();
+}
+
+const addStudentToCourse = async()=>{
 const res = await courseRequest.addStudentToCourse(id);
-console.log(res);
+
 
 if (res.data.success) {
   setCourse({...course, isStudentEnrolled:true})
@@ -102,14 +140,9 @@ if (res.data.success) {
 
   };
 
-  const countLikes = async()=>{
-    const countRes =await courseRequest.countLike(id);
-    if (countRes.data.success) {
-      setCourse({...course, count: countRes.data.result})
-    }
-  }
 
-  return {course, likeCourse, disLikeCourse, addStudentToCourse, countLikes}
+
+  return {course, toggleLike, addStudentToCourse}
 
 }
 
--- src/pages/course.tsx
+++ src/pages/course.tsx
@@ -3,29 +3,16 @@ import React from 'react';
 import { useFetchCourseById } from '../features/Courses/hooks/useFetchCourseById';
 import CoursePanel from '../containers/CoursePanel';
 
-export interface CoursePageProps {}
-const btnStyle =
-  'bg-slate-400 hover:bg-slate-500 text-slate-100 p-2 rounded-md';
-const CoursePage: React.FunctionComponent<CoursePageProps> = () => {
-  const { id } = useParams() as { id: string };
-  const { course, likeCourse, disLikeCourse, addStudentToCourse } =
-    useFetchCourseById(id);
+import LoadingAnimation from '../components/loadingAnimation/loadingAnimation';
 
-  return course.payload ?(<CoursePanel {...course.payload} />
-    // <div className="w-full bg-slate-200 gap-3 p-2 flex flex-col min-h-screen">
-    //   <h1>Course_{course.status}</h1>
-    //   <button onClick={likeCourse} className={btnStyle}>
-    //     like
-    //   </button>
-    //   <button onClick={disLikeCourse} className={btnStyle}>
-    //     disLike
-    //   </button>
-    //   <button onClick={addStudentToCourse} className={btnStyle}>
-    //     Enroll
-    //   </button>
-    //   <div className="bg-zinc-300 p-2 rounded-md">{`likeCount: ${course.count.like}`}</div>
-    // </div>
-  ) : <></>;
+export interface CoursePageProps {}
+
+const CoursePage: React.FunctionComponent<CoursePageProps> = () => {
+  const {id} = useParams() as {id: string};
+  const {course, toggleLike, addStudentToCourse} = useFetchCourseById(id) ;
+
+  return course.status === 'succeeded' ?(<CoursePanel course={course} toggleLike={toggleLike} addStudentToCourse={addStudentToCourse} />
+  ) : <div className='w-screen min-h-screen relative flex justify-center items-center' ><LoadingAnimation/></div> ;
 };
 
 export default CoursePage;
